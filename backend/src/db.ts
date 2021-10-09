85e649d265a68661c990c52832fa012477bfdb69
estephany <28768405+estephanypaniagua@users.noreply.github.com>
2021-10-08 23:36:55 -0500

    feat: db connection

---
+++ backend/src/db.ts
@@ -0,0 +1,14 @@
+import { connect } from "mongoose";
+
+// const URI = "tipo_db://user:password@host:puerto/database";
+// const URI = "tipo_db://host:puerto/database";
+const URI = "mongodb://localhost:27017/test";
+
+const startConnection = async () => {
+    try {
+        await connect(URI);
+        console.log("Conexion con la base de datos exitosa");    
+    } catch (er) {
+        console.log("Error con la base de datos");
+    }
+};
